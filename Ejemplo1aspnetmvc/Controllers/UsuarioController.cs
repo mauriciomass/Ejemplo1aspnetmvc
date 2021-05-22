72d776a7c74da880e52ee1e79ce7fde8b35a82e2
mauriciomass <mauriciomass@misena.edu.co>
2021-05-22 06:51:12 -0500

    consultando datos de la tabla usuario

---
+++ Ejemplo1aspnetmvc/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ejemplo1aspnetmvc.Models;
+
+namespace Ejemplo1aspnetmvc.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.usuario.ToList());
+            }
+        }
+    }
+}
