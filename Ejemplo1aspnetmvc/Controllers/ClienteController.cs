c6da06bd35f933b8fc43864c4b654f91bd0ef9d3
mauriciomass <mauriciomass@misena.edu.co>
2021-05-24 20:31:42 -0500

    Creación de formulario de los clientes

---
+++ Ejemplo1aspnetmvc/Controllers/ClienteController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ejemplo1aspnetmvc.Models;
+
+namespace Ejemplo1aspnetmvc.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+
+                return View(db.cliente.ToList());
+            }
+            
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(cliente cliente)
+        {
+            if(!ModelState.IsValid)
+
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.cliente.Add(cliente);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+
+                }
+            }catch (Exception ex) 
+            {
+                ModelState.AddModelError("", " error " + ex);
+                return View();
+            }
+
+        }
+
+    }
+}
