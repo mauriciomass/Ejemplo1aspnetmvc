39be770872bdc80ec3e3323104f9aa13e6fe4380
mauriciomass <mauriciomass@misena.edu.co>
2021-05-24 20:39:43 -0500

    Creación formulario para proveedores

---
+++ Ejemplo1aspnetmvc/Controllers/ProveedorController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ejemplo1aspnetmvc.Models;
+
+namespace Ejemplo1aspnetmvc.Controllers
+{
+    public class ProveedorController : Controller
+    {
+        // GET: Proveedor
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+
+                return View(db.proveedor.ToList());
+            }
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(proveedor proveedor)
+        {
+            if (!ModelState.IsValid)
+
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.proveedor.Add(proveedor);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", " error " + ex);
+                return View();
+            }
+
+        }
+    }
+}
